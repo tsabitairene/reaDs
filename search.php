7eac5e75ca0c2ffb86248f9da9e448b280ed7a57
tsabitairene <tsabita.irene31@gmail.com>
2024-05-31 20:36:25 +0700

    search

--- search.php
+++ search.php
@@ -7,13 +7,13 @@ $keyword = isset($_GET['cari']) ? $_GET['cari'] : '';
 
 // Prepare SQL query based on the presence of keyword
 if ($keyword) {
-    $sql = "SELECT * FROM buku WHERE judul_buku LIKE ? OR kategori LIKE ?";
-    $param = "%$keyword%";
-    $stmt = $is_connect->prepare($sql);
-    $stmt->bind_param("ss", $param, $param);
+  $sql = "SELECT * FROM buku WHERE judul_buku LIKE ? OR kategori LIKE ?";
+  $param = "%$keyword%";
+  $stmt = $is_connect->prepare($sql);
+  $stmt->bind_param("ss", $param, $param);
 } else {
-    $sql = "SELECT * FROM buku";
-    $stmt = $is_connect->prepare($sql);
+  $sql = "SELECT * FROM buku";
+  $stmt = $is_connect->prepare($sql);
 }
 
 $stmt->execute();
@@ -88,26 +88,26 @@ $result = $stmt->get_result();
         </ul>
 
         <div class="d-flex align-items-center ml-auto">
-                <!-- Search Form -->
-                <form id="search" action="search.php" method="GET" class="search-input d-flex align-items-center mr-4" style="position: relative;">
-                  <input type="text" placeholder="cari buku" id='searchText' name="cari" style="padding-left: 20px;" />
-                  <button type="submit" name="search" style="border: none; background: none;"><i class="fa-solid fa-magnifying-glass" style="color:black; position: absolute; right: 55px; top: 50%; transform: translateY(-50%);"></i></button>
-                </form>
+          <!-- Search Form -->
+          <form id="search" action="search.php" method="GET" class="search-input d-flex align-items-center mr-4" style="position: relative;">
+            <input type="text" placeholder="cari buku" id='searchText' name="cari" style="padding-left: 20px;" />
+            <button type="submit" name="search" style="border: none; background: none;"><i class="fa-solid fa-magnifying-glass" style="color:black; position: absolute; right: 55px; top: 50%; transform: translateY(-50%);"></i></button>
+          </form>
 
-                <ul class="js-clone-nav d-none d-lg-inline-block site-menu d-flex align-items-center">
-                    <li class="mr-3">
-                        <img src="images/badge.png" alt="">
-                    </li>
-                    <li class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fa-solid fa-user"></i>
-                        </button>
-                        <div class="dropdown-menu" style="border-radius: 20px; " aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="profile.php">Profile</a>
-                            <a href="logout.php" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-                        </div>
-                    </li>
-                </ul>
+          <ul class="js-clone-nav d-none d-lg-inline-block site-menu d-flex align-items-center">
+            <li class="mr-3">
+              <img src="images/badge.png" alt="">
+            </li>
+            <li class="dropdown">
+              <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa-solid fa-user"></i>
+              </button>
+              <div class="dropdown-menu" style="border-radius: 20px; " aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="profile.php">Profile</a>
+                <a href="logout.php" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
+              </div>
+            </li>
+          </ul>
         </div>
 
         <a href="#" class="burger ml-auto site-menu-toggle js-menu-toggle d-inline-block dark d-lg-none" data-toggle="collapse" data-target="#main-navbar">
@@ -117,48 +117,49 @@ $result = $stmt->get_result();
     </div>
   </nav>
 
-  <div class="untree_co-section">
-    <div class="container">
-      <div class="row">
-    </div> <!-- /.container -->
-  </div> <!-- /.untree_co-section -->
-
-  <div class="untree_co-section">
-    <div class="container">
-
-      <div class="row mb-5">
-        <div class="col-12 text-center" data-aos="fade-up" data-aos-delay="0">
-          <!-- <span class="caption">Features</span> -->
-          <h2 class="heading">hasil search dari : <?php echo htmlspecialchars($keyword); ?></h2>
-          <!-- <p>Minggu ini</p> -->
+    <div class="untree_co-section">
+      <div class="container">
+        <div class="row">
         </div>
-      </div> <!-- /.row -->
+      </div> <!-- /.container -->
+    </div> <!-- /.untree_co-section -->
 
-    </div> <!-- /.container -->
-  </div> <!-- /.untree_co-section -->
+    <div class="untree_co-section">
+      <div class="container">
 
-  <div class="card-container">
-    <?php while($row = $result->fetch_assoc()): ?>
-      <div class="card" style="width: 10rem; border: 1px solid #ccc; border-radius: 20px;">
-        <img src="img/<?php echo $row['gambar']; ?>" class="card-img-top" alt="..." style="height: 250px; width: 100%;">
-        <div class="card-body d-flex flex-column">
-          <div class="mb-auto">
-            <span class="caption"><?php echo $row['kategori']; ?></span>
-            <h5 class="card-title"><?php echo $row['judul_buku']; ?></h5>
+        <div class="row mb-5">
+          <div class="col-12 text-center" data-aos="fade-up" data-aos-delay="0">
+            <!-- <span class="caption">Features</span> -->
+            <h2 class="heading">hasil search dari : <?php echo htmlspecialchars($keyword); ?></h2>
+            <!-- <p>Minggu ini</p> -->
           </div>
-          <a href="detail.php?id=<?php echo $row['id']; ?>" class="btn btn-primary" style="display: grid; place-items: center;">Detail</a>
+        </div> <!-- /.row -->
+
+      </div> <!-- /.container -->
+    </div> <!-- /.untree_co-section -->
+
+    <div class="card-container">
+      <?php while ($row = $result->fetch_assoc()) : ?>
+        <div class="card" style="width: 10rem; border: 1px solid #ccc; border-radius: 20px;">
+          <img src="img/<?php echo $row['gambar']; ?>" class="card-img-top" alt="..." style="height: 250px; width: 100%;">
+          <div class="card-body d-flex flex-column">
+            <div class="mb-auto">
+              <span class="caption"><?php echo $row['kategori']; ?></span>
+              <h5 class="card-title"><?php echo $row['judul_buku']; ?></h5>
+            </div>
+            <a href="detail.php?id=<?php echo $row['id']; ?>" class="btn btn-primary" style="display: grid; place-items: center;">Detail</a>
+          </div>
         </div>
-      </div>
-    <?php endwhile; ?>
-  </div>
-        
+      <?php endwhile; ?>
+    </div>
+
     <?php
     // Close statement and connection
     $stmt->close();
     $is_connect->close();
     ?>
 
-        
+
     <div class="untree_co-section">
       <div class="container">
         <div class="row">
@@ -174,42 +175,41 @@ $result = $stmt->get_result();
       <div class="row">
         <div class="col-lg-4">
           <div class="widget">
-            <a href="index.html" class="logo m-0">rea<span class="text-primary">D</span>s</a>
-            <p>Aplikasi pinjam buku yang menarik.</p>
+            <a href="index.php" class="logo m-0">rea<span class="text-primary">D</span>s</a>
+            <p>Bersama reaDs, pinjam buku menjadi lebih seru dan menyenangkan.</p>
           </div> <!-- /.widget -->
           <div class="widget">
             <h3>Follow Us</h3>
             <ul class="list-unstyled social">
-              <li><a href="#"><span class="icon-instagram"></span></a></li>
-              <li><a href="#"><span class="icon-twitter"></span></a></li>
-              <li><a href="#"><span class="icon-facebook"></span></a></li>
-              <li><a href="#"><span class="icon-linkedin"></span></a></li>
-              <li><a href="#"><span class="icon-pinterest"></span></a></li>
-              <li><a href="#"><span class="icon-dribbble"></span></a></li>
+              <li><a href="https://www.instagram.com/stembayolib"><span class="icon-instagram"></span></a></li>
+              <!-- <li><a href="#"><span class="icon-twitter"></span></a></li> -->
+              <li><a href="https://www.facebook.com/stembayolover"><span class="icon-facebook"></span></a></li>
+              <li><a href="https://www.linkedin.com/school/smk-n-2-depok-sleman-yogyakarta/"><span class="icon-linkedin"></span></a></li>
+              <!-- <li><a href="#"><span class="icon-pinterest"></span></a></li> -->
+              <!-- <li><a href="#"><span class="icon-dribbble"></span></a></li> -->
             </ul>
           </div> <!-- /.widget -->
         </div> <!-- /.col-lg-3 -->
 
         <div class="col-lg-2 ml-auto">
           <div class="widget">
-            <!-- <h3>Projects</h3> -->
+            <h3>Page</h3>
             <ul class="list-unstyled float-left links">
               <li><a href="#">Home</a></li>
               <li><a href="#">Kategori</a></li>
               <li><a href="#">History</a></li>
-              <li><a href="#">Contact</a></li>
             </ul>
           </div> <!-- /.widget -->
         </div> <!-- /.col-lg-3 -->
 
         <div class="col-lg-2">
           <div class="widget">
-            <!-- <h3>Services</h3> -->
+            <h3>Services</h3>
             <ul class="list-unstyled float-left links">
-              <li><a href="#">Built for Developers</a></li>
-              <li><a href="#">Modern Design</a></li>
-              <li><a href="#">Building Stunning Websites</a></li>
-              <li><a href="#">Bring Ideas To Life</a></li>
+              <li><a href="#">Discover</a></li>
+              <li><a href="#">Borrow</a></li>
+              <li><a href="#">Read</a></li>
+              <li><a href="#">Your Gateway to Knowledge!</a></li>
             </ul>
           </div> <!-- /.widget -->
         </div> <!-- /.col-lg-3 -->
@@ -218,10 +218,10 @@ $result = $stmt->get_result();
         <div class="col-lg-3">
           <div class="widget">
             <!-- <h3></h3> -->
-            <address>SMK Negeri 2 Depok</address>
+            <address><a href="https://maps.app.goo.gl/qx5d234AWkCP7vGz5">Jl. STM Pembangunan, Mrican, Caturtunggal, Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</a></address>
             <ul class="list-unstyled links mb-4">
-              <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-              <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+              <li><a href="tel://11234567890">0274 - 513515</a></li>
+              <!-- <li><a href="tel://11234567890">+1(123)-456-7890</a></li> -->
               <li><a href="mailto:info@mydomain.com">reads@gmail.com</a></li>
             </ul>
           </div> <!-- /.widget -->
@@ -231,17 +231,20 @@ $result = $stmt->get_result();
 
       <div class="row mt-5">
         <div class="col-12 text-center">
-          <p class="copyright">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> <!-- License information: https://untree.co/license/ -->
+          <p class="copyright"><a href="index.php">reaDs</a> &copy;<script>
+              document.write(new Date().getFullYear());
+            </script>. All Rights Reserved. &mdash; Designed with love by Kelompok 7 <!-- License information: https://untree.co/license/ -->
           </p>
         </div>
       </div>
     </div> <!-- /.container -->
   </div> <!-- /.site-footer -->
 
-  <div id="overlayer"></div>
-    <div class="loader">
-      <div class="spinner-border" role="status">
-        <span class="sr-only">Loading...</span>
+    <div id="overlayer"></div>
+      <div class="loader">
+        <div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
       </div>
     </div>
   </div>
@@ -256,9 +259,9 @@ $result = $stmt->get_result();
   <script src="js/jquery.waypoints.min.js"></script>
   <script src="js/jquery.fancybox.min.js"></script>
   <script src="js/aos.js"></script>
-  
+
   <script src="js/custom.js"></script>
-  
+
 </body>
 
 </html>
